63980d041aa0d184369ba1dc02f518edaa4ed2f8
Joao Felipe Stein <joao.stein.of@gmail.com>
2025-09-29 22:53:12 +0000

    Refatorando apos teste 8 : Refatoracao simples.

--- velha.cpp
+++ velha.cpp
@@ -21,25 +21,31 @@ int VerificaVelha(int velha[3][3]) {
     int returnValue = -1;
 
     for (int column = 0; column < 3; column++) {
-       if (velha[0][column] != 0 && velha[0][column] == velha[1][column] &&
-         velha[1][column] == velha[2][column]) {
-         returnValue = velha[0][column];
-       }
+		bool columnIsEqual = (velha[0][column] == velha[1][column]) 
+						&& (velha[1][column] == velha[2][column]);
+
+        if (velha[0][column] != 0 && columnIsEqual) {
+         	returnValue = velha[0][column];
+        }
     }
 
     for (int line = 0; line < 3; line++) {
-       if (velha[line][0] != 0 && velha[line][0] == velha[line][1] &&
-         velha[line][1] == velha[line][2]) {
-         returnValue = velha[line][0];
-       }
+		bool lineIsEqual = (velha[line][0] == velha[line][1]) 
+						&& (velha[line][1] == velha[line][2]);
+
+        if (velha[line][0] != 0 && lineIsEqual) {
+			returnValue = velha[line][0];
+        }
     }
 
-	if (velha[0][0] != 0 && velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2])
+	bool diagonalIsEqual = (velha[0][0] == velha[1][1]) && (velha[1][1] == velha[2][2]);
+	if (velha[0][0] != 0 && diagonalIsEqual)
 	{
 		returnValue = velha[0][0];
 	}
 
-	if (velha[0][2] != 0 && velha[0][2] == velha[1][1] && velha[1][1] == velha[2][0])
+	bool antiDiagonalIsEqual = (velha[0][2] == velha[1][1]) && (velha[1][1] == velha[2][0]);
+	if (velha[0][2] != 0 && antiDiagonalIsEqual)
 	{
 		returnValue = velha[0][2];
 	}
